f1216c354901840a7b8f48b2fe322faaee0fe7f8
HaydarZaraki <haydargaily@gmail.com>
2022-07-21 01:36:11 +0400

    Ingestion + NY Taxi Data

--- Docker_Sql/pipeline.py
+++ Docker_Sql/pipeline.py
@@ -2,9 +2,10 @@ import sys
 
 import pandas as pd
 
-#sys.argv is used for specifying the arguments passed with the file when running the container
+# sys.argv is used for specifying the arguments passed with the file when running the container
 print(sys.argv)
-
+if len(sys.argv) > 2:
+    raise RuntimeError("Too many arguments")
 # First Argument sys.argv[0] by default is "pipeline.py"
 # Second argument sys.argv[1] is the day of execution
 
