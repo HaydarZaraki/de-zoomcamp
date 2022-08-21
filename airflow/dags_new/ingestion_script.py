c4d4b72077bba14abbc673191969a06adea43c41
Chief <haydargaily@gmail.com>
2022-08-21 14:23:54 +0400

    Airflow

---
+++ airflow/dags_new/ingestion_script.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def ingest_callable(user,host,password,port,db,table_name,parquet_file):
+    data_file_name = parquet_file
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    engine.connect()
+
+    df = pd.read_parquet(data_file_name, engine='auto')
+    print(pd.io.sql.get_schema(df.reset_index(), name=table_name, con=engine))
+
+    df.to_sql(name=table_name, con=engine, chunksize=100000, if_exists='replace')
+    print(df.head())
+
+
--- airflow/dags_new/local_ingestion_dag.py
+++ airflow/dags_new/local_ingestion_dag.py
@@ -2,25 +2,38 @@ import os
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+from ingestion_script import ingest_callable
 
-url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet'
-
+url_head = 'https://d37ci6vzurychx.cloudfront.net/trip-data'
+url_template = url_head + '/yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet'
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+output_file = AIRFLOW_HOME+'/output_{{execution_date.strftime(\'%Y-%m\')}}.parquet'
+
+PG_USERNAME = os.environ.get(PG_USERNAME)
+PG_PASSWORD =os.environ.get(PG_PASSWORD)
+PG_HOST =os.environ.get(PG_HOST)
+PG_PORT =os.environ.get(PG_PORT)
+PG_DATABASE =os.environ.get(PG_DATABASE)
 
 with DAG(
-        'Download Parquet + List Directories',
+        'Ingestion',
         description='A simple tutorial DAG to showcase Airflow basics',
         schedule_interval='0 6 2 * *',
-        start_date=datetime(2022, 1, 1),
+        start_date=datetime(2021, 1, 1),
 ) as dag:
     wget_task = BashOperator(
         task_id='wget',
-        bash_command=f'curl -sS {url} > {AIRFLOW_HOME}/output.parquet'
+        bash_command=f'curl -sS {url_template} > {output_file}'
     )
 
-    ingest_task = BashOperator(
-        task_id='ingest',
-        bash_command=f'ls -al {AIRFLOW_HOME}'
+    ingest_task = PythonOperator(
+        task_id="ingest",
+        python_callable=ingest_callable,
+        op_kwargs={
+            "user": BUCKET,
+            "object_name": f"raw/{parquet_file}",
+            "local_file": f"{path_to_local_home}/{parquet_file}",
+        },
     )
 
     wget_task >> ingest_task
