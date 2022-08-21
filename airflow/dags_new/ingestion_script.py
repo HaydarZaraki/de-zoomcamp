475fc0bc74d04918f9d94ddb4bbba6d2334dc7cd
HaydarZaraki <haydarguetta@gmail.com>
2022-08-21 11:59:57 +0000

    [Airflow] Docker-Compose Fix

--- airflow/dags_new/ingestion_script.py
+++ airflow/dags_new/ingestion_script.py
@@ -7,11 +7,12 @@ def ingest_callable(user,host,password,port,db,table_name,parquet_file):
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     engine.connect()
-
+    print("Connection Established , Inserting data ...................")
     df = pd.read_parquet(data_file_name, engine='auto')
     print(pd.io.sql.get_schema(df.reset_index(), name=table_name, con=engine))
 
     df.to_sql(name=table_name, con=engine, chunksize=100000, if_exists='replace')
     print(df.head())
+    print("Data Inserted Successfully !!")
 
 
--- airflow/dags_new/local_ingestion_dag.py
+++ airflow/dags_new/local_ingestion_dag.py
@@ -1,6 +1,7 @@
 import os
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 from ingestion_script import ingest_callable
 
@@ -8,12 +9,13 @@ url_head = 'https://d37ci6vzurychx.cloudfront.net/trip-data'
 url_template = url_head + '/yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet'
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 output_file = AIRFLOW_HOME+'/output_{{execution_date.strftime(\'%Y-%m\')}}.parquet'
+table_name = 'yellow_taxi_{{execution_date.strftime(\'%Y_%m\')}}'
 
-PG_USERNAME = os.environ.get(PG_USERNAME)
-PG_PASSWORD =os.environ.get(PG_PASSWORD)
-PG_HOST =os.environ.get(PG_HOST)
-PG_PORT =os.environ.get(PG_PORT)
-PG_DATABASE =os.environ.get(PG_DATABASE)
+PG_USERNAME = os.environ.get("PG_USERNAME")
+PG_PASSWORD = os.environ.get("PG_PASSWORD")
+PG_PORT = os.environ.get("PG_PORT")
+PG_HOST = os.environ.get("PG_HOST")
+PG_DATABASE = os.environ.get("PG_DATABASE")
 
 with DAG(
         'Ingestion',
@@ -29,11 +31,15 @@ with DAG(
     ingest_task = PythonOperator(
         task_id="ingest",
         python_callable=ingest_callable,
-        op_kwargs={
-            "user": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
-        },
+        op_kwargs=dict(
+        user=PG_USERNAME,
+        host=PG_HOST,
+        password=PG_PASSWORD,
+        port=PG_PORT,
+        db=PG_DATABASE,
+        table_name=table_name,
+        parquet_file=output_file
+        ),
     )
 
     wget_task >> ingest_task
