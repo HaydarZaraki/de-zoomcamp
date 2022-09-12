b1028d95e601ae1fc160e5e0394440e4dd0b763e
HaydarZaraki <haydarguetta@gmail.com>
2022-09-12 12:28:36 +0000

    GCS t->External t-> Partitioned

--- airflow/dags/yellow_green_gcs_2_part.py
+++ airflow/dags/yellow_green_gcs_2_part.py
@@ -6,14 +6,15 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 
 # Our airflow operators
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
 
 # Helps us to interact with GCP Storage
 from google.cloud import storage
 
 # To allow us to interact with BigQuery
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator,BigQueryInsertJobOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+
 
 # Helps to convert our data to parquet format
 import pyarrow.csv as pv
@@ -22,11 +23,11 @@ import pyarrow.parquet as pq
 # Take environmental variables into local variables. These were set in the docker-compose setup.
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
-
-dataset_file = "yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
-dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "ny_taxi")
+# dataset_file = "yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
+# dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-yellow_gcp_storage_template = "raw/yellow_taxi_data/{{execution_date.strftime(\'%Y\')}}/yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
+# yellow_gcp_storage_template = "raw/yellow_taxi_data/{{execution_date.strftime(\'%Y\')}}/yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
 
 def upload_to_gcs(bucket, object_name, local_file):
     client = storage.Client()
@@ -43,30 +44,42 @@ default_args = {
 }
 
 with DAG(
-    dag_id="data_ingestion_gcs_dag_v02",
-    schedule_interval="@monthly",
+    dag_id="yellow_green_gcs_to_external_partitioned",
+    schedule_interval=None,
     default_args=default_args,
-    catchup=True,
-    start_date=datetime(2019, 1, 1),
-    end_date=datetime(2021,1,1),
+    start_date=days_ago(1),
     max_active_runs=3,
     tags=['dtc-de'],
 ) as dag:
+    for color,partition in {'yellow':'tpep_pickup_datetime','green':'lpep_pickup_datetime'}.items():
+        bq_external_table_task = BigQueryCreateExternalTableOperator(
+        task_id=f"{color}_bq_external_table_task",
+        table_resource={
+            "tableReference": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": f"{color}_ext_table",
+            },
+            "externalDataConfiguration": {
+                "sourceFormat": "PARQUET",
+                "sourceUris": [f"gs://{BUCKET}/{color}/*.parquet"],
+                },
+            },
+        )
+        exclude_columns = ''
+        if color == 'yellow':
+            exclude_columns = 'except(airport_fee)'
+        else:
+            exclude_columns = 'except(ehail_fee)'
+        QUERY = f"CREATE OR REPLACE TABLE `{PROJECT_ID}.ny_taxi.{color}_taxi_part` PARTITION BY DATE({partition}) AS SELECT * {exclude_columns} FROM `{PROJECT_ID}.ny_taxi.{color}_ext_table`;"
+        part_cluster_table_task = BigQueryInsertJobOperator(
+            task_id=f"{color}_part_cluster_table_task",
+            configuration={
+                "query": {
+                    "query": QUERY,
+                    "useLegacySql": False,
+                }
+            },
+        )
 
-    download_dataset_task = BashOperator(
-        task_id="download_dataset_task",
-        bash_command=f"curl -sSLf {dataset_url} > {path_to_local_home}/{dataset_file}"
-    )
-
-
-    local_to_gcs_task = PythonOperator(
-        task_id="local_to_gcs_task",
-        python_callable=upload_to_gcs,
-        op_kwargs={
-            "bucket": BUCKET,
-            "object_name": yellow_gcp_storage_template,
-            "local_file": f"{path_to_local_home}/{dataset_file}",
-        },
-    )
-
-    download_dataset_task >> local_to_gcs_task
+        bq_external_table_task >> part_cluster_table_task
